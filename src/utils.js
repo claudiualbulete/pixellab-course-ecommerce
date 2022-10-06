0a296df6c4aa8273706df6909bf3e7f46ea595b0
albuletec <claudiu.albulete@paddypowerbetfair.com>
2022-10-06 21:18:45 +0300

    Changes: * Filters adjustments

--- src/utils.js
+++ src/utils.js
@@ -9,5 +9,5 @@
 export const getCurrency = (price, locale = 'en-US', currency = 'USD') => {
     const intlNumber = new Intl.NumberFormat(locale, { style: 'currency', currency }).format(price);
 
-    return intlNumber.toLocaleString(locale);
+    return intlNumber.toLocaleString();
 }
