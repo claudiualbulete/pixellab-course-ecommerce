3eea3337ce83c34f75cfe3b3e8f67674ef4aff99
albuletec <claudiu.albulete@paddypowerbetfair.com>
2022-10-06 22:33:14 +0300

    Changes: * fix filters

--- src/components/catalog/FiltersUsed.jsx
+++ src/components/catalog/FiltersUsed.jsx
@@ -1,7 +1,7 @@
 export const FiltersUsed = () => {
   return (
-    <section className="border h-16 w-48 lg:h-20 flex items-center justify-center">
-      filters used: 12
+    <section className="border h-16 w-36 lg:h-20 flex items-center justify-center">
+      Filters: 12
     </section>
   )
 }
